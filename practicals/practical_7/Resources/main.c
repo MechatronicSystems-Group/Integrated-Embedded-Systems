5ff54bfc640e3df1bf5d7ded28375129357963af
james-hepworth <81862393+james-hepworth@users.noreply.github.com>
2025-05-11 17:16:32 +0200

    Update prac7 main.c

--- practicals/practical_7/Resources/main.c
+++ practicals/practical_7/Resources/main.c
@@ -65,10 +65,18 @@ void main (void)
 */
 void set_to_48MHz(void)
 {
-    RCC->CFGR |= (0b1010 << 18);                // Set PLL multiplier to 12
-    RCC->CFGR |= RCC_CFGR_SW_PLL;               // Set PLL as system clock
-
-    RCC->CR |= RCC_CR_PLLON;                    // Turn on PLL clock
+    if ((RCC->CFGR & RCC_CFGR_SWS) == RCC_CFGR_SWS_PLL)
+    {
+        RCC->CFGR &= (uint32_t) (~RCC_CFGR_SW);
+        while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);
+    }
+    RCC->CR &= (uint32_t)(~RCC_CR_PLLON);
+    while ((RCC->CR & RCC_CR_PLLRDY) != 0);
+    RCC->CFGR = ((RCC->CFGR & (~0x003C0000)) | 0x00280000);
+    RCC->CR |= RCC_CR_PLLON;
+    while ((RCC->CR & RCC_CR_PLLRDY) == 0);
+    RCC->CFGR |= (uint32_t) (RCC_CFGR_SW_PLL);
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
 }
 
 
